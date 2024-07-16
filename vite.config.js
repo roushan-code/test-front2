c1fdc3615f15fce85d85e250eb7dfb4fede5ac5d
Roushan <roushansingh3698@gmail.com>
2024-07-17 02:30:52 +0530

    add plugin

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
+import image from '@rollup/plugin-image'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { createHtmlPlugin } from 'vite-plugin-html'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  
-  plugins: [react()],
+  plugins: [
+    react(),
+    image(),
+    createHtmlPlugin({
+      entry: 'src/main.jsx',
+      template: '/index.html',
+      inject: {
+        data: {
+          title: 'index',
+          injectScript: `<script src="./inject.js"></script>`,
+        },
+      }
+    })
+  ]
 })
