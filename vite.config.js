d945e08efa807f36e1773a78f437c71c3543bbf9
Roushan <roushansingh3698@gmail.com>
2024-07-18 19:24:57 +0530

    build plugin for vite rollup-plugin-visualizer

--- vite.config.js
+++ vite.config.js
@@ -16,6 +16,19 @@ export default defineConfig({
           injectScript: `<script src="./inject.js"></script>`,
         },
       }
-    })
-  ]
+    }),
+  ],
+  build: {
+    report: true, // Enable build report
+    chunkSizeWarningLimit: 500, // adjust the limit to 500KB
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          header: ['src/component/layout/Header/Header.jsx'],
+          footer: ['src/component/layout/Footer/Footer.jsx'],
+        },
+      },
+    },
+  }
 })
